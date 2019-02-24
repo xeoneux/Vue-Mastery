0ad2c830fd86909dde4bcbdf7faf7b711f1554d5
Aayush Kapoor <xeoneux@icloud.com>
2019-02-24 20:59:12 +0530

    1 - Use computed properties

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,20 +2,22 @@ var app = new Vue({
   el: "#app",
   data: {
     cart: 0,
-    inStock: true,
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
+    brand: "Vue Mastery",
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
       }
     ]
   },
@@ -23,8 +25,19 @@ var app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariants].variantQuantity;
     }
   }
 });
