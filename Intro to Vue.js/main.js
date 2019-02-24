6b1f9bdb0eeb0746a63eef3aa636c038dfa0c476
Aayush Kapoor <xeoneux@icloud.com>
2019-02-24 12:01:27 +0530

    1 - List rendering of details

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -1,8 +1,13 @@
 var app = new Vue({
   el: "#app",
   data: {
-    inventory: 8,
+    inStock: true,
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg"
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      { variantId: 2234, variantColor: "green" },
+      { variantId: 2235, variantColor: "blue" }
+    ]
   }
 });
