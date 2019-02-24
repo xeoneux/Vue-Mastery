53b237950db60b90da72df38972c7d68ab96d474
Aayush Kapoor <xeoneux@icloud.com>
2019-02-24 21:18:01 +0530

    1 - Register product as component

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -1,25 +1,63 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    cart: 0,
-    product: "Socks",
-    brand: "Vue Mastery",
-    selectedVariant: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg",
-        variantQuantity: 10
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-        variantQuantity: 0
-      }
-    ]
+Vue.component("product", {
+  template: `
+    <div class="product">
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div
+          class="color-box"
+          :key="variant.variantId"
+          v-for="(variant, index) in variants"
+          @mouseover="updateProduct(index)"
+          :style="{ backgroundColor: variant.variantColor }"
+        ></div>
+
+        <button
+          @click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+        >
+          Add to Cart
+        </button>
+
+        <div class="cart">
+          <p>Cart({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./assets/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0
+        }
+      ]
+    };
   },
   methods: {
     addToCart() {
@@ -37,7 +75,11 @@ var app = new Vue({
       return this.variants[this.selectedVariant].variantImage;
     },
     inStock() {
-      return this.variants[this.selectedVariants].variantQuantity;
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
+});
+
+var app = new Vue({
+  el: "#app"
 });
