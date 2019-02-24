bde7522a105ba5ffcd7dbb0dca5db76f6482e9d7
Aayush Kapoor <xeoneux@icloud.com>
2019-02-24 22:29:58 +0530

    1 - Refactor cart list

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -63,7 +63,7 @@ Vue.component("product", {
   },
   methods: {
     addToCart() {
-      this.$emit("add-to-cart");
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].variantId);
     },
     updateProduct(index) {
       this.selectedVariant = index;
@@ -91,12 +91,12 @@ Vue.component("product", {
 var app = new Vue({
   el: "#app",
   data: {
-    cart: 0,
+    cart: [],
     premium: true
   },
   methods: {
-    updateCart() {
-      this.cart += 1;
+    updateCart(id) {
+      this.cart.push(id);
     }
   }
 });
